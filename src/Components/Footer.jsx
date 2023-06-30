84287336acb3b87d2e91deb7fcf1acb985bfaee6
Samuel Harris <samkeepscoding25@gmail.com>
2023-06-30 15:26:32 -0400

    Change Footer bg to black

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className='flex justify-center items-center py-8 text-center '>
+    <footer className='flex justify-center items-center py-8 text-center bg-black'>
         <p className='text-sm font-extralight'>TrackTeam Productions LLC @</p>
     </footer>
   )
