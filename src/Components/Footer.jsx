82c63665f2c7c7058f46032b8a96e3e2b4a0fe35
Samuel Harris <samkeepscoding25@gmail.com>
2023-07-01 19:09:47 -0400

    Title/Footer/Logo Changed

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer className='flex justify-center items-center py-8 text-center bg-black text-white'>
-        <p className='text-sm font-extralight'>TrackTeam Productions LLC @</p>
+        <p className='text-sm font-extralight'>TrackTeam Productions LLC</p>
     </footer>
   )
 }
