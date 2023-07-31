058adcbb4b703ad13ea8310aeb419c4216e10f2b
Samuel Harris <samkeepscoding25@gmail.com>
2023-07-31 12:22:55 -0400

    Date Updated to august 9th

--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Countdown from 'react-countdown';
 
 const Counter = () => {
-    const date = new Date(2023, 7, 2, 12, 0, 0)
+    const date = new Date(2023, 7, 9, 12, 0, 0)
   return (
     <div className=' flex flex-col space-y-10 w-full h-screen justify-start items-center text-xl bg-[#ffffff] text-black pt-36'>
         <h1 className='font-bold text-3xl'>COMING SOON</h1>
