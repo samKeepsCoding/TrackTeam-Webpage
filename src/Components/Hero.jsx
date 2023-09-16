70b878159441bff89c26fc94623eabf21a5efdaf
Samuel Harris <samkeepscoding25@gmail.com>
2023-09-16 13:04:06 -0400

    Gold Silver & Copper content update

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -12,15 +12,15 @@ const Hero = () => {
         >
             <div className=' mt-36 flex flex-col justify-center items-center text-center '>
                 {/* <img src={TitleImg} alt="TrackTeam Name" /> */}
-                <h2 className='text-5xl md:text-8xl font-bold text-secondary '>SKYWALKERS</h2>
-                <div className='flex flex-row justify-between font-thin text-xs md:text-sm text-start w-full'> 
+                <h2 className='text-3xl md:text-6xl font-bold text-secondary '>GOLD SILVER & COPPER</h2>
+                <div className='flex flex-row justify-between font-thin text-xs md:text-sm text-start w-full px-4'> 
                     <p className=''>TrackTeam</p>
                     <p>X</p>
-                    <p>THE LETTER M</p>
+                    <p>Nedlog</p>
                     <p>X</p>
-                    <p>RENO BASQUIAT</p>
+                    <p>Boregard</p>
                 </div>
-                <a href="https://soundcloud.com/trackte_am">
+                <a href="https://soundcloud.com/trackte_am/gold-silver-copper/s-FQADOlEZCrf?si=523891c2a3964ff99eeb68c31e8b7a25&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing">
                     <button
                         className='mt-8  py-3 border-secondary border font-bold bg-secodaryBg 
                         text-white text-semibold px-3 shadow-sm hover:bg-secondary duration-150'
