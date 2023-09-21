a06ae159a6aa0295c1402dc713ed4e581438d356
Samuel Harris <samkeepscoding25@gmail.com>
2023-09-21 12:27:22 -0400

    Watch More button added to Header

--- src/Components/LatestVideo.jsx
+++ src/Components/LatestVideo.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const LatestVideo = () => {
   return (
-    <div className='w-full flex justify-center items-center p-8'>
+    <div className='w-full flex justify-center items-center p-8' id='latestVideo'>
         <div className='w-full max-w-6xl bg-black text-white flex flex-col items-center space-y-4 py-8 '>
             <h1 className='text-start flex-wrap w-full text-3xl md:text-7xl font-extrabold text-secondary'>WATCH 'GOLD SILVER & COPPER FT. NEDLOG & BOREGARD'</h1>
             <div className='w-full flex justify-center'>
--- src/Components/VideoBG.jsx
+++ src/Components/VideoBG.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
-import vid from '../Assets/promoWide.mp4'
+import vid from '../Assets/promoWide.mp4';
+import { Link } from 'react-scroll';
+
 
 const VideoBG = () => {
+
+
+
+
   return (
     <div className='w-full h-screen text-white'>
         <div className='absolute w-full h-full top-0 flex flex-col justify-center items-center'></div>
-        <video src={vid} autoPlay loop muted className='w-full h-full object-cover'/>
+        <video src={vid} controls={false} autoPlay loop muted className='w-full h-full object-cover'/>
         <div className='absolute w-full h-full top-0 flex flex-col justify-center items-center '>
         <h2 className='text-3xl md:text-6xl lg:text-8xl font-extrabold text-secondary '>GOLD SILVER & COPPER</h2>
-            <div className='flex flex-row justify-between font-thin text-xs md:text-sm text-start w-full px-4 max-w-lg'> 
+            <div className='flex flex-row justify-center space-x-5 md:space-x-0 md:justify-between font-thin text-xs md:text-sm text-start w-full px-4 max-w-lg'> 
                 <p className=''>TrackTeam</p>
                 <p>X</p>
                 <p>Nedlog</p>
@@ -23,6 +29,12 @@ const VideoBG = () => {
                     LISTEN HERE
                 </button>
             </a>
+
+            <p className='font-extrabold mt-6'>OR</p>
+
+            <Link to='latestVideo' smooth offset={85} className=' px-3 py-1 cursor-pointer flex space-x-4 items-center justify-center font-bold mt-6 hover:scale-125 duration-100 ease-linear hover:text-secondary'>
+                WATCH VIDEO
+            </Link>
         </div>
     </div>
   )
